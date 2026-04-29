e73682f482acfe1081cca97d3dfc059819353875
Cessalan <31516973+Cessalan@users.noreply.github.com>
2026-04-29 17:35:33 -0400

    added clarity

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import clarity from '@microsoft/clarity';
 import App from './App';
 import { AuthProvider } from './Contexts/AuthContext/AuthContext';
 import { ProgressProvider } from './Contexts/ProgressContext/ProgressContext';
 import './i18n/i18n'; // important for translation
+
+clarity.init('wjgn4x5pp6');
 
 const root = createRoot(document.getElementById('root'));
 
